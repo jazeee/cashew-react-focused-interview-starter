224c3ed4644125539414e448ed1c2d33869685bf
Andrew Spencer <spencerandrew23@gmail.com>
2022-08-01 15:03:51 -0700

    fix frontend issue with node modules getting overwritten

--- src/client/start_frontend.sh
+++ src/client/start_frontend.sh
@@ -1,5 +1,5 @@
 docker network create flashcards
+
 docker build -f Dockerfile -t flashcards_frontend .
-docker run --name flashcards_frontend -it -v ${PWD}:/app -d -p 4001:3000 flashcards_frontend
-docker exec -d -it flashcards_frontend npm start
+docker run --name flashcards_frontend -it -v ${PWD}/src:/app/src -d -p 4001:3000 flashcards_frontend
 docker network connect flashcards flashcards_frontend
