a8c85beb8ebd341fd04144e0ae52ca724b315a40
Andrew Spencer <spencerandrew23@gmail.com>
2022-08-01 22:07:00 -0700

    fix the node modules being wiped away

--- src/server/start_backend.sh
+++ src/server/start_backend.sh
@@ -1,6 +1,8 @@
 docker build -f Dockerfile -t flashcards_backend .
 docker run --name flashcards_database -e POSTGRES_PASSWORD=cashew -e POSTGRES_USER=postgres -e POSTGRES_DATABASE=flashcards -d -p 127.0.0.1:5433:5432 postgres
 docker run --name flashcards_backend -it -v ${PWD}:/app -d -p 4002:4000 flashcards_backend
+docker exec -it flashcards_backend npm install
+
 
 docker cp database.sql flashcards_database:/
 
